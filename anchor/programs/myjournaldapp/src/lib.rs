d5d96ef1bd47b1e6bc29003eafeb7f9f7e130b72
matthew-lim-matthew-lim <matthewlimcenterdiv@gmail.com>
2024-12-14 00:46:15 +1100

    add instruction handler, and CreateEntry struct and handler

--- anchor/programs/myjournaldapp/src/lib.rs
+++ anchor/programs/myjournaldapp/src/lib.rs
@@ -5,66 +5,42 @@ use anchor_lang::prelude::*;
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
-pub mod myjournaldapp {
-    use super::*;
+pub mod journal {
+  user super::*;
 
-  pub fn close(_ctx: Context<CloseMyjournaldapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.myjournaldapp.count = ctx.accounts.myjournaldapp.count.checked_sub(1).unwrap();
-    Ok(())
-  }
-
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.myjournaldapp.count = ctx.accounts.myjournaldapp.count.checked_add(1).unwrap();
-    Ok(())
-  }
-
-  pub fn initialize(_ctx: Context<InitializeMyjournaldapp>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.myjournaldapp.count = value.clone();
-    Ok(())
-  }
-}
-
-#[derive(Accounts)]
-pub struct InitializeMyjournaldapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + Myjournaldapp::INIT_SPACE,
-  payer = payer
-  )]
-  pub myjournaldapp: Account<'info, Myjournaldapp>,
-  pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseMyjournaldapp<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-  )]
-  pub myjournaldapp: Account<'info, Myjournaldapp>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub myjournaldapp: Account<'info, Myjournaldapp>,
+  // Each journal entry has its own PDA. Every new entry means new account initialised on-chain.
+  // With Anchor, every instruction takes a Context type as its first argument. Context macro is used to define a struct 
+  // that contains all the accounts that will be passed through a given instruction handler.
+  pub fn create_entry(
+    ctx: Context<CreateEntry>,  // CreateEntry is a custom data structure for this instruction.
+  )
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Myjournaldapp {
-  count: u8,
+pub struct JournalEntryState {
+  pub owner: Pubkey,
+  #[max_len(20)]
+  pub title: String,
+  #[max_len(2000)]
+  pub message: String,
+  pub entry_id: u64,  // Helpful for indexing on FE
+}
+
+// Deserialise and validate the list of accounts specified within the struct. 
+#[derive(Accounts)]
+#[instruction(title: String)] // Instruction macro allows data to be passed past the instruction handler
+pub struct CreateEntry<'info> {
+  #[account(
+    init,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    payer = owner, 
+    space = 8 + JournalEntryState::INIT_SPACE,
+  )]
+  // Define the account that will be created by this instruction.
+  pub journal_entry: Account<'info, JournalEntryState>,
+  #[account(mut)] // The account is mutable because the owner will be making a payment.
+  pub owner: Signer<'info>,
+  pub system_program: Program<'info, System>,
 }
